c12aeb0539ee3ceafbcc47a5142d06bd617bf82b
Pikachuxxxx <phani.s2909@icloud.com>
2021-02-10 18:48:12 +0530

    Updated premake to work with all configurations

--- premake5.lua
+++ premake5.lua
@@ -1,43 +1,52 @@
 project "ImGui"
-	kind "StaticLib"
-	language "C++"
+    kind "StaticLib"
+    language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
 
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp",
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
         "imgui_tables.cpp"
-	}
+    }
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
         cppdialect "C++17"
-		staticruntime "On"
+        staticruntime "On"
 
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
 
-	filter "configurations:Dist"
-            symbols "Off"
-            optimize "Full"
+    filter "configurations:Dist"
+        runtime "Release"
+        symbols "Off"
+        optimize "Full"
